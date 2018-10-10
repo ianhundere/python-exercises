cec480128eaf672be4dd4d2ba28bcf0ac1863d20
Ian Hundere <ianfundere@gmail.com>
2018-10-10 14:50:53 -0400

    adds guessnumber.py

--- python104/guessnumber.py
+++ python104/guessnumber.py
@@ -1,11 +1,12 @@
-secretNumber = 5
+import random
 
-while guess != secretNumber:
-    guess = int(input("Guess a number between 1 and 10: ")
+secretNumber = random.randint(1, 10)
 
+guess = int(input("Guess a number between 1 and 10: "))
 
-    
-    if guess == >1 and > 10:
-        print(int(input("Guess a number between 1 and 10: ")
-    elif guess == secretNumber: 
-        print("you guessed right!")
+while not guess == secretNumber:
+    if guess < 1 or guess > 10:
+        print("Input a number from 1 to 10")
+    guess = int(input("Guess a number between 1 and 10: "))
+
+print("You guessed right!")
